3c8a641d1c1ea84186786040b776cfabb172f2e3
Brandon Smith <vectorcell@gmail.com>
2016-11-23 06:01:00 -0600

    added logging, time module without seconds updating (for lower CPU usage)

--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ import sys
 import os
 import time
 import datetime
-import threading
+import string
 import urllib.request
 import Adafruit_CharLCD as LCD
 
@@ -53,12 +53,29 @@ def get_sighandler(lcd):
     return sighandler
 
 
+def printlog(*t, **d):
+    print('[{}]'.format(time.strftime('%Y-%m-%d_%H:%M:%S')), end=' ')
+    def str_to_bytearray(s):
+        if not isinstance(s, str):
+            return s
+        for c in s:
+            if c not in string.printable:
+                return bytearray(s, 'utf-8')
+        return s
+    def reform_bytearray(ba):
+        if not isinstance(ba, bytearray):
+            return ba
+        return str(ba)[len('bytearray('):][:-1]
+    tcopy = tuple(reform_bytearray(str_to_bytearray(e)) for e in t)
+    print(*tcopy, **d)
+
+
 class Module:
 
     def __init__(self, lcd):
         raise NotImplementerError('this is an abstract constructor')
 
-    def update(self):
+    def update(self, force = False):
         raise NotImplementerError('update is an abstract method')
 
     def show(self):
@@ -67,6 +84,80 @@ class Module:
     def hide(self):
         raise NotImplementerError('update is an abstract method')
 
+
+
+class ModuleTime(Module):
+
+    def __init__(self, lcd):
+        self.lcd = lcd
+        self.cache = None
+        self.last_update = 0
+        self.visible = False
+
+    def __need_update(self):
+        # always
+        return True
+        # every second
+        #return self.visible and (not self.cache or (int(time.time()) > (self.last_update)))
+
+    def update(self, force = False):
+        if self.__need_update() or force:
+            new = time.strftime('%Y %b %d, %a\n%-I:%M:%S %p')
+            if new == self.cache:
+                return False
+            self.cache = new
+            self.last_update = int(time.time())
+            return True
+        return False
+
+    def show(self):
+        if not self.visible:
+            printlog('show time')
+            self.lcd.clear()
+            self.visible = True
+        self.update()
+        self.lcd.home()
+        #self.lcd.set_color(*WHITE)
+        self.lcd.message(self.cache)
+
+    def hide(self):
+        self.visible = False
+
+
+class ModuleTimeNoSeconds(Module):
+
+    def __init__(self, lcd):
+        self.lcd = lcd
+        self.cache = None
+        self.last_update = 0
+        self.visible = False
+
+    def __need_update(self):
+        # every minute
+        return self.visible and (not self.cache or (int(time.time()) > (self.last_update + 60)))
+
+    def update(self, force = False):
+        if self.__need_update() or force:
+            new = time.strftime('%Y %b %d, %a\n%-I:%M %p')
+            if new == self.cache:
+                return False
+            self.cache = new
+            self.last_update = int(time.time())
+            return True
+        return False
+
+    def show(self):
+        if not self.visible:
+            printlog('show time (no seconds)')
+            self.lcd.clear()
+            self.visible = True
+        self.update()
+        self.lcd.home()
+        #self.lcd.set_color(*WHITE)
+        self.lcd.message(self.cache)
+
+    def hide(self):
+        self.visible = False
 
 
 class ModuleWeather(Module):
@@ -82,8 +173,8 @@ class ModuleWeather(Module):
         # every 15 minutes
         return self.visible and (not self.cache or (int(time.time()) > (self.last_update + 15 * 60)))
 
-    def update(self):
-        if self.__need_update():
+    def update(self, force = False):
+        if self.__need_update() or force:
             self.lcd.clear()
             #self.lcd.set_color(*YELLOW)
             self.lcd.message('LOADING WEATHER')
@@ -101,8 +192,10 @@ class ModuleWeather(Module):
                     temperatures += [isolate_text(line)]
                 elif 'myforecast-current' in line:
                     conditions += [isolate_text(line)]
-            output = '\n'.join((' '.join(temperatures), ' '.join(conditions)))
+            output = '\n'.join((' '.join(temperatures).strip(), ' '.join(conditions).strip()))
+            output = ''.join(c for c in output if c in string.printable)
             output = output.replace('&deg;', '°').replace('°', '\x01')
+            printlog(output)
             self.cache = output
             self.last_update = int(time.time())
             return True
@@ -110,48 +203,12 @@ class ModuleWeather(Module):
 
     def show(self):
         if not self.visible:
+            printlog('show weather')
             self.lcd.clear()
-        self.visible = True
+            self.visible = True
         self.update()
         #self.lcd.set_color(*CYAN)
         self.lcd.clear()
-        self.lcd.message(self.cache)
-
-    def hide(self):
-        self.visible = False
-
-
-class ModuleTime(Module):
-
-    def __init__(self, lcd):
-        self.lcd = lcd
-        self.cache = None
-        self.last_update = 0
-        self.visible = False
-
-    def __need_update(self):
-        # always
-        return True
-        # every second
-        #return self.visible and (not self.cache or (int(time.time()) > (self.last_update)))
-
-    def update(self):
-        if self.__need_update():
-            new = time.strftime('%Y %b %d, %a\n%-I:%M:%S %p')
-            if new == self.cache:
-                return False
-            self.cache = new
-            self.last_update = int(time.time())
-            return True
-        return False
-
-    def show(self):
-        if not self.visible:
-            self.lcd.clear()
-        self.visible = True
-        self.update()
-        self.lcd.home()
-        #self.lcd.set_color(*WHITE)
         self.lcd.message(self.cache)
 
     def hide(self):
@@ -172,9 +229,10 @@ def main():
     colors = (RED, GREEN, BLUE, YELLOW, CYAN, MAGENTA, WHITE, BLACK)
     color_idx = 0
 
-    modules = (ModuleTime(lcd), ModuleWeather(lcd))
-    mod_idx = 0
+    modules = (ModuleTime(lcd), ModuleTimeNoSeconds(lcd), ModuleWeather(lcd))
+    mod_idx = 1
     stopped = False
+    force_update = False
     delay = 0.1
     modules[mod_idx].show()
     while not stopped:
@@ -189,6 +247,10 @@ def main():
                 if mod_idx < 0:
                     mod_idx = len(modules) - 1
             modules[mod_idx].show()
+        elif lcd.is_pressed(LCD.LEFT) and lcd.is_pressed(LCD.RIGHT):
+            printlog('stop')
+            stopped = True
+            continue
         elif lcd.is_pressed(LCD.LEFT) or lcd.is_pressed(LCD.RIGHT):
             if lcd.is_pressed(LCD.LEFT):
                 color_idx -= 1
@@ -198,17 +260,20 @@ def main():
                 color_idx += 1
                 if color_idx == len(colors):
                     color_idx = 0
+            printlog('set color:', ''.join(e for e in globals() if globals()[e] == colors[color_idx]))
             lcd.set_color(*colors[color_idx])
             time.sleep(0.25)
             continue
         elif lcd.is_pressed(LCD.SELECT):
-            stopped = True
-            continue
+            printlog('force update to current module')
+            force_update = True
         time.sleep(delay)
 
         # request module update every iteration
-        if modules[mod_idx].update():
+        if modules[mod_idx].update(force = force_update):
             modules[mod_idx].show()
+
+        force_update = False
 
     exit_blank(lcd)
 
